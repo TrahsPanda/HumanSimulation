15a1bb70980f847616bad17917b2fab9fbb024a0
TrahsPanda <zupdike17@gmail.com>
2022-01-14 16:27:39 -0500

    Final update
    Note: Broken and final. I am rewriting it

---
+++ ConsoleReadWrite.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simulation;
+
+namespace Simulation
+{
+    public class ConsoleReadWrite
+    {
+        public int yearInput;
+        public long humanStat;
+
+        public static void Main(string[] args)
+        {
+            Pregnancy pregnancy = new Pregnancy();
+
+            Console.WriteLine("Hello and welcome to TrahsPanda's Simulation!\nPlease enter the amount of Humans you want to start with:");
+            long humanStat = Int64.Parse(Console.ReadLine());
+            Console.WriteLine("How many years would you like the simulation to run?");
+            int yearInput = Int32.Parse(Console.ReadLine());
+
+            while (pregnancy.year <= yearInput)
+            // Prints current year's stats
+            Console.WriteLine("\nYEAR " + pregnancy.year);
+            Console.Write("POPULATION: ");
+            Console.WriteLine(humanStat.ToString("N0"));
+            Console.Write("BIRTHS: ");
+            Console.WriteLine(pregnancy.pregnancyStat.ToString("N0"));
+            Console.Write("DEATHS: ");
+            Console.WriteLine(pregnancy.babyDeathStat.ToString("N0"));
+        }
+    }
+}
---
+++ Pregnancy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Simulation;
+
+namespace Simulation
+{
+    public class Pregnancy
+    {
+        public int year;
+        public int pregnancyStat;
+        public int babyDeathStat;
+        public int twinStat;
+        public int currentlyPregnant;
+        public static void PregnancyClass(string[] args)
+        {
+            ConsoleReadWrite consoleReadWrite = new ConsoleReadWrite();
+            Random random = new Random();
+            int year = 1;
+            int pregnancyStat = 0;
+            int babyDeathStat = 0;
+            int twinStat = 0;
+            int currentlyPregnant = 0;
+
+            while (year <= consoleReadWrite.yearInput)
+            {
+
+                // REPRODUCTION
+                if (consoleReadWrite.humanStat > 1)
+                {
+                    // humanCouple represnts every human coming together as a couple
+                    long humanCouple = consoleReadWrite.humanStat / 2;
+                    for (int x = 0; x < humanCouple; x++)
+                    {
+                        // This if statement checks if all criteria is met to breed
+                        if (random.NextDouble() > 0.5)
+                        {
+                            // Twins and solo pregnancies
+                            if (random.NextDouble() < 0.004)
+                            {
+                                currentlyPregnant += currentlyPregnant + 2;
+                                pregnancyStat++;
+                                twinStat++;
+                            }
+                            else
+                            {
+                                currentlyPregnant++;
+                                pregnancyStat++;
+                            }
+
+                            // Runs through every pregnancy
+                            for (int y = 0; y < currentlyPregnant; y++)
+                            {
+                                // Chance to kill a child
+                                if (random.NextDouble() < 0.45)
+                                {
+                                    babyDeathStat++;
+                                    currentlyPregnant--;
+                                }
+                            }
+                            consoleReadWrite.humanStat += currentlyPregnant;
+                            currentlyPregnant = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Whatever you entered was not correct.");
+                    Environment.Exit(0);
+                }
+                year++;
+            }
+        }
+
+    }
+}
