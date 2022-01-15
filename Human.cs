89f55e17f693c535336fe2c20334e7cb8fa1e626
TrahsPanda <zupdike17@gmail.com>
2022-01-15 16:27:59 -0500

    2.0

---
+++ Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation
+{
+
+    class Human
+    {
+        internal static ulong nextID = 1;
+        public static ulong ID = nextID++;
+
+        public string Name = "Human " + nextID;
+    }
+
+}
