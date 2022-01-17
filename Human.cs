c613c7063b6be96389aa5227976e9fea44fbc618
TrahsPanda <zupdike17@gmail.com>
2022-01-16 21:20:57 -0500

    2.9

--- Human.cs
+++ Human.cs
@@ -9,34 +9,27 @@ namespace Simulation
 
     class Human
     {
-        public Random random = new Random(); // I'm gonna move this somewhere later
+        internal static Random random = new Random();
         // Basic ID system for Humans; Thanks Evelyn
         internal static ulong nextID = 1;
         public ulong ID = nextID++;
 
         public int age;
         public string name;
-        string _gender;
-        public string gender;
-        public void Gender()
+        internal static int genderCount = Enum.GetValues(typeof(Genders)).Length;
+        public Genders gender = (Genders)random.Next(genderCount);
+        public enum Genders
         {
-            if (random.NextDouble() > 0.5)
-            {
-                _gender = "Female";
-
-            }
-            else
-            {
-                _gender = "Male";
-            }
-
+            Male = 0,
+            Female = 1,
+            //NonBinary = 3,
         }
         // What every Human is
         public Human()
         {
             name = "Human " + ID;
             age = 1;
-            gender = _gender;
+            Genders gender; // gender Declared but never used
         }
 
     }
